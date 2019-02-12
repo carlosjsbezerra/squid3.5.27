85b29cf1397015813619c0cea18611a141343661
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2019-02-12 16:23:45 -0300

    Update squid3.sh
    sdf

--- squid3.sh
+++ squid3.sh
@@ -1,66 +1,71 @@
 #!/bin/bash
 #
-
+# variaveis
+vermelho="\033[0;31m"
+amarelo="\033[1;33m"
+verde="\033[0;32m"
+fimcor="\033[0m"
 #Remova os caracteres espúrios de CR. Você pode fazer isso com o seguinte comando
 # sed -i -e 's/\r$//' <nome do arquivo>
 
-# Exportando o recurso de Noninteractive do Debconf para não solicitar telas de configuração
+echo -e "$marelo Exportando o recurso de Noninteractive do Debconf para não solicitar telas de configuração $fimcor"
+sleep 5
 export DEBIAN_FRONTEND="noninteractive"
-#
-# Script de instalação do SQUID3 no GNU/Linux Ubuntu 18.04.x
+sleep 5
+echo
 # opção do comando echo: -e (enable interpretation of backslash escapes)
 # opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
 echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n"
+sleep 5
 
 #
 
-echo -e "Instalação do SQUID3 no GNU/Linux ubuntu 18.04.x"
-echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet....\n"
+echo -e "$amarelo Aguarde, esse processo demora um pouco dependendo do seu Link de Internet....$fimcor\n"
 sleep 5
 #
-echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
+echo -e "$amarelo Adicionando o Repositório Universal do Apt, aguarde...$fimcor"
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	add-apt-repository universe
-echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
+echo -e "$verde Repositório adicionado com sucesso!!!, continuando com o script...$fimcor"
 sleep 5
 echo
 #
-echo -e "Adicionando o Repositório Multiversão do Apt, aguarde..."
+echo -e "$amarelo Adicionando o Repositório Multiversão do Apt, aguarde... $fimcor"
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	add-apt-repository multiverse
-echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
+echo -e "$verde Repositório adicionado com sucesso!!!, continuando com o script...$fimcor"
 sleep 5
 echo
 #
-echo -e "Atualizando as listas do Apt, aguarde..."
+echo -e "$amarelo Atualizando as listas do Apt, aguarde...$fimcor"
 	#opção do comando: &>> (redirecionar a entrada padrão)
 	sleep 5
 	apt update
 	clear
-echo -e "Listas atualizadas com sucesso!!!, continuando com o script..."
+echo -e "$verde Listas atualizadas com sucesso!!!, continuando com o script...$fimcor"
 sleep 5
 echo
 #
-echo -e "Atualizando o sistema, aguarde..."
+echo -e "$amarelo Atualizando o sistema, aguarde...$fimcor"
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando apt: -y (yes)
 	sleep 5
 	apt -y upgrade
 	clear
-echo -e "Sistema atualizado com sucesso!!!, continuando com o script..."
+echo -e "$verde Sistema atualizado com sucesso!!!, continuando com o script...$verde"
 sleep 5
 echo
 #
-echo -e "Removendo software desnecessários, aguarde..."
+echo -e "$vermelho Removendo software desnecessários, aguarde...$fimcor"
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando apt: -y (yes)
 	sleep 5
 	apt -y autoremove 
-echo -e "Software removidos com sucesso!!!, continuando com o script..."
+echo -e "$verde Software removidos com sucesso!!!, continuando com o script...$verde"
 sleep 5
 clear
 #
-echo -e "Instalando o SQUID3, aguarde..."
+echo -e "$amarelo Instalando o SQUID3, aguarde...$fimcor"
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando apt: -y (yes), \ (bar left) quedra de linha na opção do apt
 	sleep 5
@@ -69,12 +74,12 @@ echo -e "Instalando o SQUID3, aguarde..."
 	# opção do comando cp: -v (verbose)
 	# opção do comando mv: -v (verbose)
 	clear
-	echo -e "Movendo o aquivo squid.conf para squid.conf.olg"
+	echo -e "$amarelo Movendo o aquivo squid.conf para squid.conf.olg $fimcor"
 	mv -v /etc/squid/squid.conf /etc/squid/squid.conf.old 
-	echo -e "Copiando a arquivo conf/squid.conf para /etc/squid/"
+	echo -e "$amarelo Copiando a arquivo conf/squid.conf para /etc/squid/ $fimcor"
 	cp -v conf/squid.conf /etc/squid/squid.conf 
 	sleep 5
-	echo -e "Editando o arquivo de configuração do SQUID.CONF, pressione <Enter> para continuar..."
+	echo -e "$vermelho Editando o arquivo de configuração do SQUID.CONF, pressione <Enter> para continuar...$fimcor"
 		read
 		sleep 3
 		vim /etc/squid/squid.conf
@@ -98,6 +103,6 @@ echo -e "Instalação do squid3 feita com Sucesso!!!, recomendado reinicializar 
 	echo -e "Tempo gasto para execução do script $0: $TEMPO"
 echo -e "Pressione <Enter> para concluir o processo."
 # opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
-echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
+echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n"
 read
 exit 1
