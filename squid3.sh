d7ce2e71dbe408c1acf9cb81e7b41dd3f5e61ed1
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2019-02-12 15:26:30 -0300

    teste
    teset

--- squid3.sh
+++ squid3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+
+#Remova os caracteres espúrios de CR. Você pode fazer isso com o seguinte comando
+# sed -i -e 's/\r$//' <nome do arquivo>
+
 # Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
 # opção do comando date: +%T (Time)
 HORAINICIAL=`date +%T`
@@ -40,3 +44,88 @@ if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
 		echo -e "Execute novamente o script para verificar o ambiente."
 		exit 1
 fi
+#
+# Script de instalação do SQUID3 no GNU/Linux Ubuntu 18.04.x
+# opção do comando echo: -e (enable interpretation of backslash escapes)
+# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
+echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
+
+#
+
+echo -e "Instalação do SQUID3 no GNU/Linux ubuntu 18.04.x"
+echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet....\n"
+sleep 5
+#
+echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
+	# opção do comando: &>> (redirecionar a entrada padrão)
+	add-apt-repository universe &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Adicionando o Repositório Multiversão do Apt, aguarde..."
+	# opção do comando: &>> (redirecionar a entrada padrão)
+	add-apt-repository multiverse &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Atualizando as listas do Apt, aguarde..."
+	#opção do comando: &>> (redirecionar a entrada padrão)
+	apt update &>> $LOG
+echo -e "Listas atualizadas com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Atualizando o sistema, aguarde..."
+	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando apt: -y (yes)
+	apt -y upgrade &>> $LOG
+echo -e "Sistema atualizado com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Removendo software desnecessários, aguarde..."
+	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando apt: -y (yes)
+	apt -y autoremove &>> $LOG
+echo -e "Software removidos com sucesso!!!, continuando com o script..."
+sleep 5
+clear
+#
+echo -e "Instalando o SQUID3, aguarde..."
+	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando apt: -y (yes), \ (bar left) quedra de linha na opção do apt
+	apt -y install squid3 &>> $LOG
+	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando cp: -v (verbose)
+	# opção do comando mv: -v (verbose)
+	mv -v /etc/squid/squid.conf /etc/squid/squid.conf.old &>> $LOG
+	cp -v conf/squid.conf.conf /etc/squid/squid.conf &>> $LOG
+	echo -e "Editando o arquivo de configuração do SQUID.CONF, pressione <Enter> para continuar..."
+		read
+		sleep 3
+		vim /etc/squid/squid.conf
+	echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
+echo -e "Atualização do SQUID3.CONF feita com sucesso!!!, continuando com o script..."
+sleep 5
+clear
+
+#
+
+echo -e "Instalação do squid3 feita com Sucesso!!!, recomendado reinicializar o servidor no final da instalação."
+	# script para calcular o tempo gasto (SCRIPT MELHORADO, CORRIGIDO FALHA DE HORA:MINUTO:SEGUNDOS)
+	# opção do comando date: +%T (Time)
+	HORAFINAL=`date +%T`
+	# opção do comando date: -u (utc), -d (date), +%s (second since 1970)
+	HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
+	HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
+	# opção do comando date: -u (utc), -d (date), 0 (string command), sec (force second), +%H (hour), %M (minute), %S (second), 
+	TEMPO=`date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S"`
+	# $0 (variável de ambiente do nome do comando)
+	echo -e "Tempo gasto para execução do script $0: $TEMPO"
+echo -e "Pressione <Enter> para concluir o processo."
+# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
+echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
+read
+exit 1
