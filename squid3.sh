67929b0c07c00915fa1ea03bd9c8404b5236cd85
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2019-02-12 16:00:20 -0300

    adfasd
    asfasdf

--- squid3.sh
+++ squid3.sh
@@ -4,51 +4,13 @@
 #Remova os caracteres espúrios de CR. Você pode fazer isso com o seguinte comando
 # sed -i -e 's/\r$//' <nome do arquivo>
 
-# Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
-# opção do comando date: +%T (Time)
-HORAINICIAL=`date +%T`
-#
-# Variáveis para validar o ambiente, verificando se o usuário e "root", versão do ubuntu e kernel
-# opções do comando id: -u (user)
-# opções do comando: lsb_release: -r (release), -s (short), 
-# opões do comando uname: -r (kernel release)
-# opções do comando cut: -d (delimiter), -f (fields)
-# opção do shell script: piper | = Conecta a saída padrão com a entrada padrão de outro comando
-# opção do shell script: acento crase ` ` = Executa comandos numa subshell, retornando o resultado
-# opção do shell script: aspas simples ' ' = Protege uma string completamente (nenhum caractere é especial)
-# opção do shell script: aspas duplas " " = Protege uma string, mas reconhece $, \ e ` como especiais
-USUARIO=`id -u`
-UBUNTU=`lsb_release -rs`
-KERNEL=`uname -r | cut -d'.' -f1,2`
-#
-# Variável do caminho do Log dos Script utilizado nesse curso (VARIÁVEL MELHORADA)
-# opções do comando cut: -d (delimiter), -f (fields)
-# $0 (variável de ambiente do nome do comando)
-LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
-#
 # Exportando o recurso de Noninteractive do Debconf para não solicitar telas de configuração
 export DEBIAN_FRONTEND="noninteractive"
-#
-# Verificando se o usuário e Root, Distribuição e >=18.04 e o Kernel >=4.15 <IF MELHORADO)
-# [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria dos erros comuns na execução
-clear
-if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
-	then
-		echo -e "O usuário e Root, continuando com o script..."
-		echo -e "Distribuição e >=18.04.x, continuando com o script..."
-		echo -e "Kernel e >= 4.15, continuando com o script..."
-		sleep 5
-	else
-		echo -e "Usuário não e Root ($USUARIO) ou Distribuição não e >=18.04.x ($UBUNTU) ou Kernel não e >=4.15 ($KERNEL)"
-		echo -e "Caso você não tenha executado o script com o comando: sudo -i"
-		echo -e "Execute novamente o script para verificar o ambiente."
-		exit 1
-fi
 #
 # Script de instalação do SQUID3 no GNU/Linux Ubuntu 18.04.x
 # opção do comando echo: -e (enable interpretation of backslash escapes)
 # opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
-echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
+echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n"
 
 #
 
@@ -58,21 +20,23 @@ sleep 5
 #
 echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
 	# opção do comando: &>> (redirecionar a entrada padrão)
-	add-apt-repository universe &>> $LOG
+	add-apt-repository universe
 echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
 echo -e "Adicionando o Repositório Multiversão do Apt, aguarde..."
 	# opção do comando: &>> (redirecionar a entrada padrão)
-	add-apt-repository multiverse &>> $LOG
+	add-apt-repository multiverse
 echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
 echo -e "Atualizando as listas do Apt, aguarde..."
 	#opção do comando: &>> (redirecionar a entrada padrão)
-	apt update &>> $LOG
+	sleep 5
+	apt update
+	clear
 echo -e "Listas atualizadas com sucesso!!!, continuando com o script..."
 sleep 5
 echo
@@ -80,7 +44,9 @@ echo
 echo -e "Atualizando o sistema, aguarde..."
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando apt: -y (yes)
-	apt -y upgrade &>> $LOG
+	sleep 5
+	apt -y upgrade
+	clear
 echo -e "Sistema atualizado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
@@ -88,7 +54,8 @@ echo
 echo -e "Removendo software desnecessários, aguarde..."
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando apt: -y (yes)
-	apt -y autoremove &>> $LOG
+	sleep 5
+	apt -y autoremove 
 echo -e "Software removidos com sucesso!!!, continuando com o script..."
 sleep 5
 clear
@@ -96,12 +63,17 @@ clear
 echo -e "Instalando o SQUID3, aguarde..."
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando apt: -y (yes), \ (bar left) quedra de linha na opção do apt
-	apt -y install squid3 &>> $LOG
+	sleep 5
+	apt -y install squid3
 	# opção do comando: &>> (redirecionar a entrada padrão)
 	# opção do comando cp: -v (verbose)
 	# opção do comando mv: -v (verbose)
-	mv -v /etc/squid/squid.conf /etc/squid/squid.conf.old &>> $LOG
-	cp -v conf/squid.conf /etc/squid/squid.conf &>> $LOG
+	clear
+	echo -e "Movendo o aquivo squid.conf para squid.conf.olg"
+	mv -v /etc/squid/squid.conf /etc/squid/squid.conf.old 
+	echo -e "Copiando a arquivo conf/squid.conf para /etc/squid/"
+	cp -v conf/squid.conf /etc/squid/squid.conf 
+	sleep 5
 	echo -e "Editando o arquivo de configuração do SQUID.CONF, pressione <Enter> para continuar..."
 		read
 		sleep 3
