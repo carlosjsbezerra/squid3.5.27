6a52b7e7899b0d68f98c8e5a04f443794b182bb3
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2019-02-12 21:08:06 -0300

    Update squid3.sh
    F

--- squid3.sh
+++ squid3.sh
@@ -2,6 +2,7 @@
 #
 # variaveis
 AMARELO="\033[1;33m"
+VERDE="\033[0;32m"
 FIMCOR="\033[0m"
 
 #Remova os caracteres espúrios de CR. Você pode fazer isso com o seguinte comando
@@ -11,45 +12,53 @@ echo -e "$AMARELO Exportando o recurso de Noninteractive do Debconf para não so
 sleep 5
 export DEBIAN_FRONTEND="noninteractive"
 
+echo
 echo -e "$AMARELO Adicionando o Repositório Universal do Apt, aguarde...$FIMCOR"
 sleep 5
 add-apt-repository universe
 add-apt-repository multiverse
 
-
+echo
 echo -e "$AMARELO Atualizando as listas do Apt, aguarde...$FIMCOR \n"
 sleep 5
 apt update
 
+echo
 echo -e "$AMARELO Atualizando o sistema, aguarde...$FIMCOR\n"
 sleep 5
 apt -y upgrade
 
+echo
 echo -e "$AMARELO Removendo software desnecessários, aguarde...$FIMCOR\n"
 sleep 5
 apt -y autoremove
 
+echo
 echo -e "$AMARELO Instalando Squid3, Aguarde...$FIMCOR\n"
 sleep 5 
 apt -y install squid3
 
+echo
 echo -e "$AMARELO Backup do aquivo do squid $FIMCOR"
 sleep 5
 mv -v /etc/squid/squid.conf /etc/squid/squid.conf.old 
 sleep 5
 
+echo
 echo -e "$AMARELO Copiando o aquivo squid.conf pré configura para /etc/squid/ $FIMCOR"
 cp -v conf/squid.conf /etc/squid/squid.conf 
 sleep 5
 
+echo
 echo -e "$AMARELO Editando o arquivo squid.conf, Pressione ENTER pra continuar...$FIMCOR"
 vim /etc/squid/squid.conf
 sleep 5
 
+echo
 echo -e "$AMARELO Atribuindo as permissões necessarias para o funcionamento do squid3 $FIMCOR"
 chown -R proxy.proxy /var/log/squid/ 
 chown -R proxy:proxy /var/spool/squid
-echo -e "$AMARELO Permissões atribuidas com sucesso $FIMCOR"
+echo -e "$VERDE Permissões atribuidas com sucesso $FIMCOR"
 sleep 5
 
 systemctl stop squid.service
