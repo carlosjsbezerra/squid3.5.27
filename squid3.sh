f8d17bd38ea6ed614b3a434f35a86633ddbc4525
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2019-02-12 21:02:52 -0300

    Update squid3.sh
    d

--- squid3.sh
+++ squid3.sh
@@ -7,18 +7,17 @@ FIMCOR="\033[0m"
 #Remova os caracteres espúrios de CR. Você pode fazer isso com o seguinte comando
 # sed -i -e 's/\r$//' <nome do arquivo>
 
-echo -e "$AMARELO Exportando o recurso de Noninteractive do Debconf para não solicitar telas de configuração $FIMCOR\n"
+echo -e "$AMARELO Exportando o recurso de Noninteractive do Debconf para não solicitar telas de configuração $FIMCOR"
 sleep 5
 export DEBIAN_FRONTEND="noninteractive"
 
-echo -e "$AMARELO Adicionando o Repositório Universal do Apt, aguarde...$FIMCOR\n"
+echo -e "$AMARELO Adicionando o Repositório Universal do Apt, aguarde...$FIMCOR"
 sleep 5
 add-apt-repository universe
-sleep 2
 add-apt-repository multiverse
 
 
-echo -e "$AMARELO Atualizando as listas do Apt, aguarde...$FIMCOR\n"
+echo -e "$AMARELO Atualizando as listas do Apt, aguarde...$FIMCOR \n"
 sleep 5
 apt update
 
@@ -34,31 +33,32 @@ echo -e "$AMARELO Instalando Squid3, Aguarde...$FIMCOR\n"
 sleep 5 
 apt -y install squid3
 
-echo -e "$AMARELO Backup do aquivo do squid $FIMCOR\n"
+echo -e "$AMARELO Backup do aquivo do squid $FIMCOR"
 sleep 5
 mv -v /etc/squid/squid.conf /etc/squid/squid.conf.old 
+sleep 5
 
-echo -e "$AMARELO Copiando o aquivo squid.conf pré configura para /etc/squid/ $FIMCOR\n"
+echo -e "$AMARELO Copiando o aquivo squid.conf pré configura para /etc/squid/ $FIMCOR"
 cp -v conf/squid.conf /etc/squid/squid.conf 
+sleep 5
 
-echo -e "$AMARELO Editando o arquivo squid.conf, Pressione ENTER pra continuar...$FIMCOR\n"
+echo -e "$AMARELO Editando o arquivo squid.conf, Pressione ENTER pra continuar...$FIMCOR"
 vim /etc/squid/squid.conf
+sleep 5
 
 echo -e "$AMARELO Atribuindo as permissões necessarias para o funcionamento do squid3 $FIMCOR"
 chown -R proxy.proxy /var/log/squid/ 
 chown -R proxy:proxy /var/spool/squid
-sleep 5
 echo -e "$AMARELO Permissões atribuidas com sucesso $FIMCOR"
-
+sleep 5
 
 systemctl stop squid.service
 echo -e "$AMARELO Criando Cache do squid $FIMCOR"
 sleep 5
 squid -z
 
-
 echo -e "$AMARELO Inicializando os Serviço do squid3 $FIMCOR"
 sleep 5
 systemctl start squid.service
 clear
-systemctl start squid.service
+systemctl status squid.service
